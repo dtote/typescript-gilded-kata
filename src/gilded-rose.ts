bb6dcea0ad1846b59bf85c7d8871ef7f588306b8
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-02 13:16:34 +0000

    Went back to previous commit cause code was getting messy + refactores all logic into switch and then into an object

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -1,5 +1,4 @@
 type BasicOperation = (variation: number) => void;
-type NameCheck = (name: string) =>  boolean;
 export interface ItemInterface {
     name: string;
     sellIn: number,
@@ -8,13 +7,9 @@ export interface ItemInterface {
     decreaseSellIn: BasicOperation;
     increaseQuality: BasicOperation;
     increaseSellIn: BasicOperation;
-    hasSameNameThat: NameCheck;
-
 }
-const names = {
-    agedBrie: 'Aged Brie',
-    backstage: 'Backstage passes to a TAFKAL80ETC concert',
-    sulfuras: 'Sulfuras, Hand of Ragnaros',
+interface Stats {
+    [key: string]: void;
 }
 export class Item implements ItemInterface {
     constructor(public name: string, public sellIn: number, public quality: number) {}
@@ -30,41 +25,48 @@ export class Item implements ItemInterface {
     increaseSellIn(variation: number): void {
         this.sellIn += variation;
     }
-    hasSameNameThat(name: string): boolean {
-        return this.name === name;
-    }
 }
 
 export class GildedRose {
+    maxBound = 50;
+    minBound = 0;
+    
     constructor(private items: Item[]) {}
-
+    updateSulfurasStats(item: Item) {
+        if (item.quality < this.maxBound) item.increaseQuality(1);
+        if ((item.sellIn + 1) < 6 && (item.quality < this.maxBound)) {
+            item.increaseQuality(2);
+        }
+        if (item.sellIn < this.minBound) item.decreaseQuality(1);
+        else if (item.sellIn > this.minBound) item.increaseQuality(1);
+    }
+    updateBackstageStats(item: Item) {
+        item.decreaseSellIn(1);
+        if (item.sellIn < this.minBound) item.decreaseQuality(1);
+        else if (item.sellIn > this.minBound) item.increaseQuality(1);
+    }
+    updateAgedBrieStats(item: Item) {
+        if (item.quality < this.maxBound) item.increaseQuality(1);
+        if ((item.sellIn + 1) < 6 && (item.quality < this.maxBound)) {
+            item.increaseQuality(2);
+        }
+        item.decreaseSellIn(1);
+        item.increaseQuality(1);
+    }
+    updateDefaultStats(item: Item) {
+        if (item.quality < this.minBound) item.decreaseQuality(1);
+        else if (item.sellIn < this.minBound) item.increaseQuality(1);
+        if (item.sellIn > this.minBound) item.increaseQuality(1);
+    }
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (!this.items[i].hasSameNameThat(names.agedBrie) && 
-                !this.items[i].hasSameNameThat(names.backstage) && 
-                this.items[i].quality > 0 && 
-                !this.items[i].hasSameNameThat(names.sulfuras)) 
-            {
-                this.items[i].decreaseQuality(1);
-            } else {
-                if (this.items[i].quality < 50) this.items[i].increaseQuality(1);
-                if (!this.items[i].hasSameNameThat(names.backstage) && 
-                    ((this.items[i].sellIn + 1) < 6) && this.items[i].quality < 50) {
-                    this.items[i].increaseQuality(2);
-                } else {
-                    this.items[i].increaseQuality(1);
-                }
+        for (const item of this.items) {
+            const updateStats: Stats = {
+                'Aged Brie': this.updateAgedBrieStats(item),
+                'Backstage passes to a TAFKAL80ETC concert': this.updateBackstageStats(item),
+                'Sulfuras, Hand of Ragnaros': this.updateSulfurasStats(item),
             }
-            if (!this.items[i].hasSameNameThat(names.sulfuras)) this.items[i].decreaseSellIn(1);
-            if (this.items[i].sellIn < 0 && !this.items[i].hasSameNameThat) {
-                if (!this.items[i].hasSameNameThat(names.backstage) && this.items[i].quality > 0 && !this.items[i].hasSameNameThat(names.sulfuras)) {
-                    this.items[i].increaseQuality(1);
-                } else {
-                    this.items[i].decreaseQuality(this.items[i].quality);
-                }
-            } else if (this.items[i].quality < 50){
-                this.items[i].increaseQuality(1);
-            }
+            const found = Object.keys(updateStats).find((key) => key === item.name);
+            found ? updateStats[item.name] : this.updateDefaultStats(item);
         }
         return this.items;
     }
